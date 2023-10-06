7a435e3b689bdc28c8fe0cc1378cebbda5db9c80
Martynova-Rezeda <garaeva.rez@yandex.ru>
2023-10-06 13:24:15 +0300

    исправлен base_url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 const BASE_MOVIE_URL = "https://api.nomoreparties.co/beatfilm-movies";
-const BASE_URL = "http://localhost:3000";
-//const BASE_URL = "https://api.movies.martynova.nomoreparties.co";
+//const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://api.movies.martynova.nomoreparties.co";
 const BASE_IMAGE_URL = "https://api.nomoreparties.co";
 const MOVIE_DURATION = 40;
 
